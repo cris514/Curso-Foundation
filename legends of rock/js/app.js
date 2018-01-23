94416daee1f93517226fbb5510e049737112f533
cris514 <cristopher.peraza@uabc.edu.mx>
2018-01-22 20:18:06 -0800

    Animacion menu movil, formspree
    Se utilizo data-drilldown, para ralizar una animacion en el menu movil y utilizamos formspree para recibir los correos de nuestros usuarios.

--- legends of rock/js/app.js
+++ legends of rock/js/app.js
@@ -6,6 +6,11 @@ $('[data-toggle-dia]').click(function (ev){
 })
 
 const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+
 $offCanvas.find('li').click(function (ev) {
   $offCanvas.foundation('close')
+  setTineout(function (){
+    $sticky.css('left','0px')
+  },300)
 })
